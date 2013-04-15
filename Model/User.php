e517c885b3a63372c7ba8e88d751976042360a4b
Ernesto Rsv <netors@gmail.com>
2013-04-15 16:00:04 -0700

    Fixed strict error on PHP 5.4 compatibility

--- Model/User.php
+++ Model/User.php
@@ -69,7 +69,7 @@ class User extends NiceAuthAppModel {
 			)
 		);
 
-	public function beforeSave() {
+	public function beforeSave($options = array()) {
     	if (isset($this->data[$this->alias]['password'])) {
         	$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
     		}
