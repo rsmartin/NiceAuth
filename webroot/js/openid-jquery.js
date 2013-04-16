52804aa481acddc05a33b93d436deb747a503d5a
Ernesto Rsv <netors@gmail.com>
2013-04-15 23:53:34 -0700

    Fixed default img_path in openid-jquery.js

--- webroot/js/openid-jquery.js
+++ webroot/js/openid-jquery.js
@@ -16,7 +16,7 @@ openid = {
 	cookie_name : 'openid_provider',
 	cookie_path : '/',
 
-	img_path : '/nice_auth/img/Fixe',
+	img_path : '/nice_auth/img/',
 	locale : null, // is set in openid-<locale>.js
 	sprite : null, // usually equals to locale, is set in
 	// openid-<locale>.js
